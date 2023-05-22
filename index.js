3ce94a74d6e535106bbe4f923b3e644bfda4d93a
frvlv6 <frvlv6@4-lab.com>
2023-05-22 18:19:46 +0900

    Kakao Handilng

--- index.js
+++ index.js
@@ -389,23 +389,28 @@ function phoneNumCnt() {
   };
 }
 
-$("#shareBtn").click(function () {
-  if (navigator.share) {
-    navigator
-      .share({
-        title: "이벤트 공유하기",
-        url: "https://superfinder-event.vercel.app/",
-      })
-      .then(function () {
-        console.log("공유에 성공했습니다.");
-      })
-      .catch(function (error) {
-        console.log("공유에 실패했습니다.", error);
-      });
-  } else if (navigator.userAgent.indexOf("KAKAOTALK") > -1) {
-    const link = "https://superfinder-event.vercel.app/";
-    prompt("아래 링크를 복사해주세요:", link);
-  } else {
-    alert("이 브라우저에서는 공유가 불가합니다.");
-  }
+$(document).ready(function () {
+  $("#shareBtn").click(function () {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "이벤트 공유하기",
+          url: "https://superfinder-event.vercel.app/",
+        })
+        .then(function () {
+          console.log("공유에 성공했습니다.");
+        })
+        .catch(function (error) {
+          console.log("공유에 실패했습니다.", error);
+        });
+    } else if (navigator.userAgent.indexOf("KAKAOTALK") > -1) {
+      // 카카오톡 인앱 브라우저
+      const link = "https://urlopen.link/superfinder-event.vercel.app/";
+      location.href = link;
+    } else {
+      // 다른 브라우저
+      const link = "https://superfinder-event.vercel.app/";
+      window.open(link, "_blank");
+    }
+  });
 });
